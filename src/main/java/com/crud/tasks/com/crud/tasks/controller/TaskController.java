9512a242d269866e8e78c20f2b9b230db8aa0601
ziembatomasz <ziembatomasz@gmail.com>
2018-09-23 18:26:45 +0200

    APIController

---
+++ src/main/java/com/crud/tasks/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.com.crud.tasks.controller;
+
+import com.crud.tasks.com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public List<TaskDto> getTasks(){
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask(Long TaskId){
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "updateTask")
+    public TaskDto updateTask(TaskDto task){
+        return new TaskDto(1L, "test title", "test_content");
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(TaskDto taskDto){
+    }
+}
