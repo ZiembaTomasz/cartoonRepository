7c3b498751a080b9bd49d6c176fa329cdedc3531
ziembatomasz <ziembatomasz@gmail.com>
2018-09-27 16:25:47 +0200

    FindById

--- src/main/java/com/crud/tasks/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/repository/TaskRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+    Task findById(final Long id);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -16,4 +16,7 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
+    public Task findById(final Long id){
+        return repository.findById(id);
+    }
 }
