1c0850730d852ac4c5a1f285427c38b9b088e936
ziembatomasz <ziembatomasz@gmail.com>
2019-04-03 20:07:47 +0100

    format

--- src/main/java/com/crud/tasks/mapper/EpisodMapper.java
+++ src/main/java/com/crud/tasks/mapper/EpisodMapper.java
@@ -10,15 +10,17 @@ import java.util.stream.Collectors;
 
 @Component
 public class EpisodMapper {
-    public Episod mapToEpisod(EpisodDto episodDto, Season season){
+    public Episod mapToEpisod(EpisodDto episodDto, Season season) {
         return new Episod(episodDto.getId(), episodDto.getName(), season);
     }
-    public EpisodDto mapToEpisodDto(Episod episod){
+
+    public EpisodDto mapToEpisodDto(Episod episod) {
         return new EpisodDto(episod.getId(), episod.getName());
     }
-    public List<EpisodDto> mapToEpisodDto(List<Episod>episods){
+
+    public List<EpisodDto> mapToEpisodDto(List<Episod> episods) {
         return episods.stream()
-                .map(t-> mapToEpisodDto(t))
+                .map(t -> mapToEpisodDto(t))
                 .collect(Collectors.toList());
     }
 
